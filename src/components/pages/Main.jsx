f588efebd9eb7c2e1e68a382ec24f9c911e8f707
JuriJuriM <jurimudry@gmail.com>
2025-09-26 10:51:00 +0300

    my commit today

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Main.scss'
+import Avatar from '../../assets/img/avatar.jpg'
 
 const Main = () => {
     return (
-        <div className='main'>Main</div>
+        <div className='main'>
+            <div className='container'>
+                <div className='main__body'>
+                    <div className='main__title'>Юрий Мудренко</div>
+                    <div className='main__row'>
+                        <div className='main__column'>
+                            <div className='main__subtitle'>Авторские книги.</div>
+                            <div className='main__subtitle'>Здесь есть, что почитать!</div>
+                        </div>
+                        <div className='main__column'>
+                            <div className='main__photo'>
+                                <img src={Avatar} alt='Avatar' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
