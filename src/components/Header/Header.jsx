91082d150890fa50b75a34e8dcf0b3dcbc17b514
JuriJuriM <jurimudry@gmail.com>
2025-10-13 14:17:04 +0300

    new day today

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
                         <nav className={`header__nav ${isOpen ? 'active' : ''}`}>
                             <ul className='header__nav-list'>
                                 <li className='header__nav-item'>
-                                    <Link to='/'>Main</Link>
+                                    <Link to='/'>Главная</Link>
                                 </li>
                                 <li className='header__nav-item'>
                                     <Link to='/second'>Second</Link>
--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Main.scss'
 import Avatar from '../../assets/img/avatar.jpg'
+import BandS from '../../assets/img/bands.png'
+import Vasilok from '../../assets/img/vasilok.png'
+import Bankomat from '../../assets/img/bancomat.png'
+import Wefromsvu from '../../assets/img/wefromsvu.png'
+import Chroniki from '../../assets/img/chroniki.png'
+import Daydospechov from '../../assets/img/daydospechov.png'
 
 const Main = () => {
     return (
@@ -11,13 +17,50 @@ const Main = () => {
                     <div className='main__row'>
                         <div className='main__column'>
                             <div className='main__subtitle'>Авторские книги.</div>
-                            <div className='main__subtitle'>Здесь есть, что почитать!</div>
+                            <div className='main__subtitle'>Здесь есть что почитать!</div>
                         </div>
                         <div className='main__column'>
                             <div className='main__photo'>
                                 <img src={Avatar} alt='Avatar' />
                             </div>
                         </div>
+                    </div>
+                    <div className='main__row-two'>
+                        <div className='main__column-two'>
+                            <div className='main__photo-two'>
+                                <img src={BandS} alt='BandS' />
+                            </div>
+                        </div>
+                        <div className='main__column-two'>
+                            <div className='main__photo-two'>
+                                <img src={Vasilok} alt='Vasilok' />
+                            </div>
+                        </div>
+                        <div className='main__column-two'>
+                            <div className='main__photo-two'>
+                                <img src={Bankomat} alt='Bankomat' />
+                            </div>
+                        </div>
+                        <div className='main__column-two'>
+                            <div className='main__photo-two'>
+                                <img src={Wefromsvu} alt='Wefromsvu' />
+                            </div>
+                        </div>
+                        <div className='main__column-two'>
+                            <div className='main__photo-two'>
+                                <img src={Chroniki} alt='Chroniki' />
+                            </div>
+                        </div>
+                        <div className='main__column-two'>
+                            <div className='main__photo-two'>
+                                <img src={Daydospechov} alt='Daydospechov' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='main__subtitle-two'>Ознакомиться с содержанием данных книг можно на сайте "Книги Юрия Мудренко".</div>
+                    <div className='main__subtitle-two'>Для перехода просто нажмите на кнопку.</div>
+                    <div className='main__button'>
+                        <a href='https://jurijurim.github.io/authorbooks/' className='main__btn' target='_blank' rel='noreferrer'>Вам сюда!</a>
                     </div>
                 </div>
             </div>
